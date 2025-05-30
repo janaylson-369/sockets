6b311dbbb105f79a0ed1941068a5bc4b4500f34d
janaylson-369 <janailson.gomes@academico.ifpb.edu.br>
2025-05-30 10:50:56 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,9 @@
-# client.py
 import socket
 
 def start_client():
     # Configuração do servidor
-    host = '127.0.0.1'  # Endereço do servidor
-    port = 12345        # Porta do servidor
+    host = '127.0.0.133'  # Endereço do servidor
+    port = 64535        # Porta do servidor
 
     # Criar um socket TCP/IP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
@@ -29,4 +28,5 @@ def start_client():
         print("Conexão encerrada")
 
 if __name__ == "__main__":
-    start_client()
+
+ start_client()
